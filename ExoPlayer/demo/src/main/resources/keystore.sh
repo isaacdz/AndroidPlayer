fd37856e957087ebae50645e7ca9fd91f0112a38
albertdaurell <albert.daurell@wuaki.tv>
2017-06-15 18:42:39 +0200

    Recompiled using a certificate for 192.168.0.XXX 192.168.1.XXX 192.168.2.XXX 10.12.100.XXX 10.12.101.XXX 10.12.102.XXX

--- ExoPlayer/demo/src/main/resources/keystore.sh
+++ ExoPlayer/demo/src/main/resources/keystore.sh
@@ -1,15 +1,18 @@
-wget https://www.bouncycastle.org/download/bcprov-ext-jdk15on-157.jar
-
 rm -rf keystore.zip keystore.jks
 
-IPS="DNS:exoplayback.wuaki.tv,IP:127.0.0.1"
-for i in {1..254}; do 
-    IPS="$IPS,IP:10.12.100.$i"
+IPS="DNS:tv.exoplayback.wuaki.tv,DNS:exoplayback.wuaki.tv,IP:127.0.0.1"
+for j in {0..2}; do
+  echo "Range 10.12.10$j.XXX"
+  for i in {1..254}; do 
+    IPS="$IPS,IP:10.12.10$j.$i"
+  done
+  echo "Range 192.168.$j.XXX"
+  for i in {1..254}; do
+    IPS="$IPS,IP:192.168.$j.$i"
+  done
 done
 
-keytool -genkey -keyalg RSA -alias playbackCA -keystore keystore.jks \
-  -providerclass org.bouncycastle.jce.provider.BouncyCastleProvider -providerpath bcprov-ext-jdk15on-157.jar \
+keytool -genkey -keyalg RSA -alias playbackCA -keystore keystore.jks -providerclass org.bouncycastle.jce.provider.BouncyCastleProvider -providerpath bcprov-ext-jdk15on-157.jar \
   -storetype BKS -validity 9999 \
   -ext SAN=$IPS
-
 zip keystore.zip keystore.jks 
